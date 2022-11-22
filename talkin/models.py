08e40acae673e0232c8e40829334b282aa531a8f
Luis Sanchez <luisanchezdh@gmail.com>
2022-11-22 10:22:21 -0500

    fixed reply mode

--- talkin/models.py
+++ talkin/models.py
@@ -7,21 +7,22 @@ class User(models.Model):
     email = models.EmailField(max_length=128)
     avatar = models.CharField(max_length=500)
     password = models.CharField(max_length=128)
-    creation_date = models.DateTimeField()
+    creation_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
       return f'{self.first_name} - {self.username}'
 
-    
-class Reply(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reply')
-    text = models.CharField(max_length=500)
-    creation_date = models.DateTimeField()
-
-    
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     text = models.CharField(max_length=500)
-    creation_date = models.DateTimeField()
+    creation_date = models.DateTimeField(auto_now_add=True)
     # reply_to = models.ForeignKey(Reply, on_delete=models.CASCADE, related_name='post', blank=True)
+    
+class Reply(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='replies')
+    smack = models.ForeignKey(Post,related_name="replies",on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    
     
